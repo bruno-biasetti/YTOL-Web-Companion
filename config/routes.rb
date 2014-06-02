a6bcd14ef77baeac3f818d481d0843519ba3d274
bruno-vk <biasetti@Brunos-MacBook-Air.local>
2014-06-02 14:38:15 -0300

    I18N PoC

--- config/routes.rb
+++ config/routes.rb
@@ -1,6 +1,6 @@
 Rails.application.routes.draw do
 
-  scope "/:locale" do
+  scope "/:locale", locale: /en-US|pt-BR|pt-PT/ do
 
     get 'prayers' => 'prayers#index'
     namespace :prayers do
@@ -11,7 +11,7 @@ Rails.application.routes.draw do
 
   # Redirect to the landing page when it's an invalid route.
   get '*path' => redirect('/')
-  
+
   root 'landing#index'
 
 end
