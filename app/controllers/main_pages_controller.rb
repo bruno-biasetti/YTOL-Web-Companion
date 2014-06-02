13971d71043047c2b9abee36b0ad16054f10c6de
bruno-vk <biasetti@Brunos-MacBook-Air.local>
2014-06-02 16:11:49 -0300

    Estrutura de navegação v0

---
+++ app/controllers/main_pages_controller.rb
@@ -0,0 +1,19 @@
+class MainPagesController < ApplicationController
+
+  def index
+    
+  end
+
+  def who_we_are
+
+  end
+
+  def contact_us
+
+  end
+
+  def oh_maria
+
+  end
+
+end
--- config/routes.rb
+++ config/routes.rb
@@ -1,6 +1,14 @@
 Rails.application.routes.draw do
 
-  scope "/:locale", locale: AVAIABLE_LOCALES do
+  scope '/:locale', locale: AVAIABLE_LOCALES do
+
+    get '/' => 'main_pages#index', as: :companion_hub
+
+    get 'who_we_are' => 'main_pages#who_we_are'
+
+    get 'contact_us' => 'main_pages#contact_us'
+
+    get 'oh_maria' => 'main_pages#oh_maria'
 
     get 'prayers' => 'prayers#index'
     namespace :prayers do
