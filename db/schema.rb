8a745304f560dd5ac76269dc7582e8a8da3454a4
Bruno Biasetti <bruno.biasetti@gmail.com>
2014-07-18 20:23:01 -0300

    Ajustes no CRUD de partilha.

--- db/schema.rb
+++ db/schema.rb
@@ -11,7 +11,17 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20140718032117) do
+ActiveRecord::Schema.define(version: 20140718224723) do
+
+  create_table "sharings", force: true do |t|
+    t.text     "spiritual_life"
+    t.text     "god_relationship"
+    t.text     "family_relationship"
+    t.text     "testimony_to_society"
+    t.integer  "user_id"
+    t.datetime "created_at"
+    t.datetime "updated_at"
+  end
 
   create_table "theme_reflections", force: true do |t|
     t.text     "question"
