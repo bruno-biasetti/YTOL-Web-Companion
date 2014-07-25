e834a7ce8ea3ba827426d941dbabd9c25fa67539
Danilo Casagrande de Almeida <dangras.almeida@gmail.com>
2014-07-25 18:47:10 -0300

    Configs Update

--- config/initializers/devise.rb
+++ config/initializers/devise.rb
@@ -10,7 +10,7 @@ Devise.setup do |config|
   # Configure the e-mail address which will be shown in Devise::Mailer,
   # note that it will be overwritten if you use your own mailer class
   # with default "from" parameter.
-  config.mailer_sender = 'bruno.biasetti@gmail.com'
+  config.mailer_sender = 'no-reply@ytolinternational.com'
   # config.mailer_sender = 'communication@ytolinternational.com'
 
   # Configure the class responsible to send e-mails.
